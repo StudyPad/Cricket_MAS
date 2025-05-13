f074ed3664401dc07f55821058162b4d162a987b
Shubham Gupta <shubham.gupta@splashlearn.com>
2025-05-14 00:00:13 +0530

    init java code

---
+++ src/test/java/com/splashlearn/mas/cricket/mainApp/MainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splashlearn.mas.cricket.mainApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

666ebf6dd680c4143fbed8732dd7e7e92bb239bc
amansethi <aman.sethi@splashlearn.com>
2025-05-21 00:07:18 +0530

    code fixes

---
+++ src/main/java/com/splashlearn/mas/cricket/models/cardAttributes/MatchesAttribute.java
@@ -0,0 +1,7 @@
+package com.splashlearn.mas.cricket.models.cardAttributes;
+
+public class MatchesAttribute extends  BaseCardAttribute{
+    public MatchesAttribute(int value) {
+        super("Matches", value, 0, 500);;
+    }
+}
